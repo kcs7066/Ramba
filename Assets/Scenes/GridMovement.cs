eb2f9e7c6a26900ceebf252d30158a5d8157aed5
kcs7066 <kcs7066@naver.com>
2025-05-13 19:29:25 +0900

    김창수 : 이동방식, 그리드 크기 수정

--- Assets/Scenes/GridMovement.cs
+++ Assets/Scenes/GridMovement.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveTime = 0.2f;
-    private bool isMoving = false;
+    public float moveTime = 0.2f; // 이동 속도
+    private bool isMoving = false; // 움직이는 중인가?
     private Vector2 input;
     private Vector3 targetPos;
     private Vector2 lookDirection = Vector2.down; // 기본 방향
@@ -12,15 +12,19 @@ public class PlayerController : MonoBehaviour
     {
         if (!isMoving)
         {
-            input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            if (Input.GetKey(KeyCode.W)) input = Vector2.up;
+            else if (Input.GetKey(KeyCode.S)) input = Vector2.down;
+            else if (Input.GetKey(KeyCode.A)) input = Vector2.left;
+            else if (Input.GetKey(KeyCode.D)) input = Vector2.right;
+            else input = Vector2.zero;
 
-            if (input.x != 0) input.y = 0;
+            // if (input.x != 0) input.y = 0; // 대각선 이동 방지
 
-            if (input != Vector2.zero)
+            if (input != Vector2.zero) // 입력이 있었다면 움직여라
             {
-                lookDirection = input;
-                targetPos = transform.position + new Vector3(input.x, input.y, 0);
-                StartCoroutine(Move());
+                lookDirection = input; // 보는 방향 = 입력방향
+                targetPos = transform.position + new Vector3(input.x * 0.5f, input.y * 0.5f, 0); //움직일 방향
+                StartCoroutine(Move()); // 움직인다
             }
 
             if (Input.GetKeyDown(KeyCode.Space))
@@ -50,7 +54,7 @@ public class PlayerController : MonoBehaviour
 
     void TryAttack()
     {
-        Vector2 attackPos = (Vector2)transform.position + lookDirection;
+        Vector2 attackPos = (Vector2)transform.position + (lookDirection/2);
 
         Collider2D hit = Physics2D.OverlapCircle(attackPos, 0.1f);
         if (hit != null)
